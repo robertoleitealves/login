c113924f65a4fa61282a95f32d5281b70fc2af07
robertoleite.rl@gmail.com <robertoleite.rl@gmail.com>
2026-01-12 17:33:18 -0300

    LOGIN COM MD5

---
+++ view/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<?php
+
+require_once "../db/db_connect.php";
+//sessão
+session_start();
+
+$erros = [];
+if (isset($_POST['entrar'])) {
+    $login = mysqli_escape_string($connect, $_POST['login']);
+    $senha = mysqli_escape_string($connect, $_POST['senha']);
+
+    if (empty($login) or empty($senha)) {
+        $erros[] = "<li>Preencha email e senha para prosseguir</li>";
+    } else {
+
+        $sql = "    SELECT  login 
+                    FROM    usuarios
+                    WHERE   login = '$login'";
+        $resultado = mysqli_query($connect, $sql);
+
+        //Verifica se consta algum registro nessa busca
+        if (mysqli_num_rows($resultado) > 0) {
+            $senha = md5($senha);
+            $sql = "SELECT  * 
+                    FROM    usuarios
+                    WHERE   login = '$login' 
+                    AND     senha = '$senha'";
+            //Variaves da conexão, query
+            $resultado = mysqli_query($connect, $sql);
+            if (mysqli_num_rows($resultado) == 1) {
+                //Converte o resultado em array e torna os dados utilizaveis
+                $dados = mysqli_fetch_array($resultado);
+                $_SESSION['logado'] = true;
+                $_SESSION['id_usuario'] = $dados['id'];
+                header('Location: home.php');
+            } else {
+                $erros[] = "<li>Usuário e senha não confere</li>";
+            }
+        } else {
+            $erros[] = "<li>Usuário inexistente</li>";
+        }
+    }
+}
+
+?>
+<html lang="pt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN</title>
+</head>
+
+<body>
+    <h1>Login</h1>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+        <label>Login: </label>
+        <input type="email" name="login" id="login">
+        <label>Senha: </label>
+        <input type="password" name="senha" id="senha">
+        <button type="submit" name="entrar">Entrar</button>
+        <div>
+            <?php
+            if (count($erros) > 0) { {
+                    foreach ($erros as $erro) {
+                        echo $erro;
+                    }
+                }
+            } ?>
+        </div>
+    </form>
+</body>
+
+</html>
